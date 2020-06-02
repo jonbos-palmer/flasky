0fade28a41020af318acfeca234b7252545c9788
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-02 14:51:49 -0500

    add bootstrap templates, add error pages

--- hello.py
+++ hello.py
@@ -10,11 +10,18 @@ bootstrap = Bootstrap(app)
 def index():
     return render_template('index.html')
 
-
-
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html')
 
 @app.route('/browser')
 def get_browser():
