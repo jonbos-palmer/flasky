a674c6e522b3690b6967d77f5fc7cfd121f20bce
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-02 11:11:17 -0500

    dynamic route with name param

--- hello.py
+++ hello.py
@@ -2,8 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Hello Fellow!</h1>'
+@app.route('/<name>')
+def index(name):
+    return f'<h1>Hello {name}!</h1>'
 
-    
