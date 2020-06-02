0b461e497dd897ef73ed1d0e07a8623e64e32ece
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-02 11:42:34 -0500

    demo of a few flask req+resp patterns

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
+from flask.helpers import make_response
 
 app = Flask(__name__)
 
-@app.route('/<name>')
+@app.route('/dynamic/<name>')
 def index(name):
     return f'<h1>Hello {name}!</h1>'
 
@@ -13,3 +14,15 @@ def get_browser():
     # neat
     user_agent=request.headers.get('User-Agent')
     return f"{user_agent}"
+
+# return a status
+@app.route('/bad')
+def get_bad_req():
+    return "<h1>Bad request</h1>", 400
+
+# create a response obj and take advantage of built in methods
+@app.route('/res-obj')
+def get_res_obj():
+    response = make_response('this is a response obj')
+    response.set_cookie('beach_boy', 'Dennis')
+    return response
