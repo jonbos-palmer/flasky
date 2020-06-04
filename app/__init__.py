52c9c9bd6af22a82d05fc36abbb749e1ab8c7e82
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-04 12:27:31 -0500

    add main blueprint folder + init, move errors to main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,13 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 bootstrap = Bootstrap()
-mail=Mail()
-moment=Moment()
+mail = Mail()
+moment = Moment()
 db = SQLAlchemy
 
+
 def create_app(config_name):
-    app=Flask(__name__)
+    app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
@@ -21,5 +22,8 @@ def create_app(config_name):
     mail.init_app(app)
     db.init_app(app)
 
+    from .main import main as main_blueprint
+
+    app.register_blueprint(main_blueprint)
 
     return app
--- hello.py
+++ hello.py
@@ -104,14 +104,6 @@ def user(name):
     return render_template('user.html', name=name)
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html')
-
-
-@app.errorhandler(500)
-def page_not_found(e):
-    return render_template('500.html')
 
 
 @app.route('/browser')
