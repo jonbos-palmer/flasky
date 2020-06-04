2f3870e70923771957f95b1a25255e41ce4086ec
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-04 12:56:58 -0500

    fix: indentation

--- config.py
+++ config.py
@@ -7,18 +7,21 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or 'b quiet'
 
 
-MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
-MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
-MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
-               ['true', 'on', '1']
-MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-FLASKY_MAIL_SUBJECT_PREFIX = '[FLASKY]'
-FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+                   ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[FLASKY]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    @staticmethod
+    def init_app(app):
+        pass
 
 class DevelopmentConfig(Config):
     DEBUG = True
