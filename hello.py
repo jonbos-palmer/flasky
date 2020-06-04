d058bc6b97b04d4bc40ad3e56006455087a033a0
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-04 12:35:03 -0500

    move views to blueprint

--- hello.py
+++ hello.py
@@ -77,58 +77,9 @@ class NameForm(FlaskForm):
 # POST, REDIRECT, GET
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    name = None
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-            if app.config['FLASKY_ADMIN']:
-                send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
-        else:
-            session['known'] = True
-
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
 
 
 
-
-@app.route('/browser')
-def get_browser():
-    # use requests as if it was a global var
-    # django requires passing req to view func
-    # neat
-    user_agent = request.headers.get('User-Agent')
-    return f"{user_agent}"
-
-# return a status
-
-
-@app.route('/bad')
-def get_bad_req():
-    return "<h1>Bad request</h1>", 400
-
-# create a response obj and take advantage of built in methods
-
-
-@app.route('/res-obj')
-def get_res_obj():
-    response = make_response('this is a response obj')
-    response.set_cookie('beach_boy', 'Dennis')
-    return response
 
 
 # Shell Context
