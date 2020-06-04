06e8d13d8ddcf273dd159245164db8183b118597
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-04 13:01:58 -0500

    fix model import

--- flasky.py
+++ flasky.py
@@ -3,7 +3,7 @@ import os
 from flask_migrate import Migrate
 
 from app import create_app, db
-from hello import User, Role
+from app.models import User, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
