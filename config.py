cbe0734224a0f577b2a871ed7e98243d643b3234
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-04 12:01:01 -0500

    fix typo, add default config option

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
 MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
 MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
 MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
-    ['true', 'on', '1']
+               ['true', 'on', '1']
 MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 FLASKY_MAIL_SUBJECT_PREFIX = '[FLASKY]'
@@ -28,16 +28,17 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'sqlite://'
+                              'sqlite://'
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:////' + \
-        os.path.join(basedir, 'data.sqlite')
+                              os.path.join(basedir, 'data.sqlite')
 
 
 config = {
-    'development' : DevelopmentConfig,
-    'testing' : TestingConfig,
-    'prodcuction' : ProductionConfig
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
 }
