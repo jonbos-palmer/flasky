bb832f1692f7f231b6550494effce58de3970e0b
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-04 13:34:06 -0500

    fix: migrate args were backwards

--- flasky.py
+++ flasky.py
@@ -7,8 +7,7 @@ from app.models import User, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
-migrate = Migrate(db, app)
-
+migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
